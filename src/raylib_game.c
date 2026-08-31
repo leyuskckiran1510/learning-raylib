b37a904eec878125046e9a912f7d070d54ad142c
leyuskckiran1510 <kdhakal1510@gmail.com>
2026-08-31 09:45:09 +0545

    fix: make the template demo work as expected

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -78,9 +78,9 @@ int main(void)
     InitAudioDevice();      // Initialize audio device
 
     // Load global data (assets that must be available in all screens, i.e. font)
-    font = LoadFont("resources/mecha.png");
+    font = LoadFont("src/resources/mecha.png");
     //music = LoadMusicStream("resources/ambient.ogg"); // TODO: Load music
-    fxCoin = LoadSound("resources/coin.wav");
+    fxCoin = LoadSound("src/resources/coin.wav");
 
     SetMusicVolume(music, 1.0f);
     PlayMusicStream(music);
